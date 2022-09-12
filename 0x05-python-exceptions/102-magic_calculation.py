5f7a5d0b900d66611ece6b7180e3e0ef73b60706
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-09-12 15:28:41 +0200

    magic

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,1 +1,15 @@
-magic
+#!/usr/bin/python3
+# 102-magic_calculation.py
+
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        except:
+            result = b + a
+            break
+    return (result)
