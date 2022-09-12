afe0e94783d7e6f293250f9779024ee27b44cc0a
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-09-12 15:24:07 +0200

    Name error exception

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,1 +1,6 @@
-#including theis and that one 
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
