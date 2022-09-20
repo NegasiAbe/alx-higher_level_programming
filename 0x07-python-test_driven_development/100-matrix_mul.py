51e439fc520158af86ca8fe93e8d9c310fee9856
Negasi Abebe <negasiabebe@gmail.com>
2022-09-20 04:46:58 -0700

    Matrix mu

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#100-matrix_mul.py
+# Negasi Abebe
+""" Define Matrix Multiplication function"""
+
+
+
---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+#101-lazy_matrix_mul.py by Negasi Abebe
+""" Define lazy_Marix Multiplication function"""
+
+
+
