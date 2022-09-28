3a8f4d546344ed4e1cb5c7bf6390d6ce15fd4164
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-09-28 18:28:06 +0200

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 #10-student.py
+"""Defines a Python class-to-JSON function."""
+
+
+def class_to_json(obj):
+    """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict__
