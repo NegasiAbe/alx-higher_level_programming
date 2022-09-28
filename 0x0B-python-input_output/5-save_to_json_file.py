809667f982be0135e75d8bde6c03bcd6e5a640d9
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-09-28 20:37:41 +0200

    save

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -3,16 +3,10 @@
 """Defines a JSON file-writing function."""
 
 
-import sys
+import json
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json"
+def save_to_json_file(my_obj, filename):
+    """Writes an object to a text file using JSON format"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
