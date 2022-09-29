510a2f6ac286ece295cdbcc5930103bf90390e8b
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-09-29 16:59:40 +0200

    inhertis from int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,15 +1,14 @@
-#!/usr/bin/python
-#100-my_int.py
-"""Defines a class MyInt that inherits from int."""
+#!/usr/bin/python3
+"""this module defines a class MyInt that inherits from int"""
 
 
 class MyInt(int):
-    """Invert int operators == and !=."""
+    """Invert int operators == and !="""
 
     def __eq__(self, value):
-        """Override == opeartor with != behavior."""
+        """Override == opeartor with != behavior"""
         return self.real != value
 
     def __ne__(self, value):
-        """Override != operator with == behavior."""
+        """Override != operator with == behavior"""
         return self.real == value
