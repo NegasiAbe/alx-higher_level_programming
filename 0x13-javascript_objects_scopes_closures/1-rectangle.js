3720c5b98dff83f2ff03e1797ea5872285105a8e
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-12-10 00:17:05 +0100

    solution added

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,1 +1,11 @@
-then after 
+#!/usr/bin/node
+/**
+ * Represents a rectangle class with width and height attributes
+ */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
