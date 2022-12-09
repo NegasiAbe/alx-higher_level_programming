68ea936f54e71728d29a7b00f1805dbc241f2e78
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-12-10 00:29:07 +0100

    done

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
-m negasi
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const reversedList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversedList.push(list[i]);
+  }
+
+  return (reversedList);
+};
