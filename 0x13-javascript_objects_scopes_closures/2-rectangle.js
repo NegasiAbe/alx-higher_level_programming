300e4665f7e58240becef222d2c343f0f01ae986
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-12-10 00:22:09 +0100

    solution added

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,13 @@
-never ever
+#!/usr/bin/node
+/**
+ * Check the parameters provided
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
