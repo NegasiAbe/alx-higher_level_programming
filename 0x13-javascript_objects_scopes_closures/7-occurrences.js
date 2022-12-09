2257cb3f50debff45c172f8c4352bca9b96f018e
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-12-10 00:28:06 +0100

    seventh solution

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,11 @@
-seventh 
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+
+  for (let i = 0; i < list.length; i++) {
+    count = (list[i] === searchElement ? count + 1 : count);
+  }
+
+  return count;
+};
